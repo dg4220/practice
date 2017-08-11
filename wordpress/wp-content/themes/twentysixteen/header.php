ea06fd569e841370837a219584c723ce5bd361c6
dg4220 <djgannon@bu.edu>
2017-08-11 08:56:09 -0400

    Added heading to theme header file

--- wordpress/wp-content/themes/twentysixteen/header.php
+++ wordpress/wp-content/themes/twentysixteen/header.php
@@ -21,7 +21,7 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?>><H1>Yeah!</H1>
 <div id="page" class="site">
 	<div class="site-inner">
 		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
